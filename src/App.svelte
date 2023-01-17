743282731521a431b18db3f01b66ec09a119de9c
grantshandy <granthandy@proton.me>
2023-01-17 10:44:47 -0700

    init

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<main class="min-h-screen bg-stone-600 text-stone-100 flex">
+  <div class="grow grid grid-cols-1 md:grid-cols-2 gap-3 p-3">
+    <div class="h-full rounded-md shadow-md p-2 bg-stone-700">
+      <textarea class="h-full rounded-md bg-stone-500 w-full p-1 font-mono text-sm"/>
+    </div>
+    <div class="h-full rounded-md shadow-md p-2 bg-stone-700">
+      <p>Preview</p>
+    <div>
+  </div>
+</main>
