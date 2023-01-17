f1da93521ff6d686bb04e01095d8ee1e1a44ce82
grantshandy <granthandy@proton.me>
2023-01-17 10:47:47 -0700

    simple update

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,14 @@
 
 <main class="min-h-screen bg-stone-600 text-stone-100 flex">
   <div class="grow grid grid-cols-1 md:grid-cols-2 gap-3 p-3">
-    <div class="h-full rounded-md shadow-md p-2 bg-stone-700">
-      <textarea class="h-full rounded-md bg-stone-500 w-full p-1 font-mono text-sm"/>
+    <div class="h-full rounded-md shadow-md p-2 bg-stone-700 flex flex-col space-y-2">
+      <textarea class="grow rounded-md bg-stone-500 w-full p-1 font-mono text-sm"/>
+      <div class="flow-root">
+        <button class="float-right rounded-md shadow-sm px-2 py-1 bg-stone-500 font-bold">Run</button>
+      </div>
     </div>
     <div class="h-full rounded-md shadow-md p-2 bg-stone-700">
-      <p>Preview</p>
+      <p>Game Preview</p>
     <div>
   </div>
 </main>
